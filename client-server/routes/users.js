9cce842242389f93c8ed337d5d1e0acd20a45329
unknown <oficinadosdrones@gmail.com>
2022-04-26 14:52:43 +0100

    fazendo PUT, DELETE, e o POST e o GETBYID

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -14,7 +14,48 @@ var router = express.Router();
 router.get('/', (req, res, next) =>  {
   client.get('/users', (err,request, response, obj) => {
       assert.ifError(err);
-      res.end(JSON.stringify(obj,null,2));
+      // res.end(JSON.stringify(obj,null,2));
+      res.json(obj);
+
+  });
+
+
+});
+/* GET users By ID a chamada do CLIENT */
+router.get('/:id', (req, res, next) =>  {
+  client.get(`/users/${req.params.id}`, (err,request, response, obj) => {
+      assert.ifError(err);
+      res.json(obj);
+
+  });
+
+
+});
+/* PUT  chamada do CLIENT,t emos que mandar o REQ.BODY q é o Objeto com os dados */
+router.put('/:id', (req, res, next) =>  {
+  client.put(`/users/${req.params.id}`, req.body, (err,request, response, obj) => {
+      assert.ifError(err);
+      res.json(obj);
+
+  });
+
+
+});
+/* DELETE  chamada do CLIENT */
+router.delete('/:id', (req, res, next) =>  {
+  client.del(`/users/${req.params.id}`, (err,request, response, obj) => {
+      assert.ifError(err);
+      res.json(obj);
+
+  });
+
+
+});
+/* POST  chamada do CLIENT */
+router.post('/', (req, res, next) =>  {
+  client.post(`/users`, req.body, (err,request, response, obj) => {
+      assert.ifError(err);
+      res.json(obj);
 
   });
 
